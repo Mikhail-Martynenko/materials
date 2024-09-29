d39c6e5f37cfb8ede74e8734838f563ddf9ae9f3
Mikhail <misha.martynenko8@yandex.ru>
2024-09-29 18:46:30 +0300

    feat: добавил хлебные крошки

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,41 @@
+<template>
+  <ol class="breadcrumbs">
+    <li v-for="(crumb, index) in breadcrumbs" :key="index">
+      <NuxtLink v-if="!isLastCrumb(index)" :to="crumb.path">
+        {{ crumb.label }} <span> / </span>
+      </NuxtLink>
+      <span v-else class="breadcrumbs__inactive">{{ crumb.label }}</span>
+    </li>
+  </ol>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  breadcrumbs: { label: string; path: string }[];
+}
+
+const props = defineProps<Props>();
+const isLastCrumb = (index: number) => index === props.breadcrumbs.length - 1;
+</script>
+
+<style scoped>
+.breadcrumbs {
+  display: flex;
+  list-style-type: none;
+  margin-bottom: 32px;
+  padding-left: 0;
+}
+
+.breadcrumbs a {
+  text-decoration: none;
+  color: #727783;
+}
+
+.breadcrumbs span {
+  margin: 0 5px;
+}
+
+.breadcrumbs__inactive {
+  color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="product-container">
+    <Breadcrumbs :breadcrumbs="breadcrumbs" />
     <h2 class="product-container__title">Комплекты стеллажных систем</h2>
 
     <FilterSort
@@ -22,8 +23,15 @@
 <script setup lang="ts">
 import {useFilterSort} from "~/hooks/useFilterSort";
 
+const breadcrumbs = ref([
+  { label: 'Главная', path: '#' },
+  { label: 'Системы хранения', path: '#' },
+  { label: 'Комплекты стеллажных систем', path: '/' },
+]);
+
 const productStore = useProductStore();
 const { items, materials } = storeToRefs(productStore);
+
 const sortOrder = ref<'asc' | 'desc'>('asc');
 const selectedMaterial = ref<string>('');
 
@@ -36,7 +44,7 @@ const filteredAndSortedItems = useFilterSort(items, sortOrder, selectedMaterial)
 .product-container {
   width: 100%;
   max-width: 1496px;
-  margin: 0 auto 103px;
+  margin: 32px auto 103px;
 }
 
 .product-container__title {
